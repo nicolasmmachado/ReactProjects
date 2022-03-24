0fd0e55f2640b264f3718212d9d0b2625ddc25a4
Nicolas Machado <82627291+nicolasmmachado@users.noreply.github.com>
2022-03-23 22:51:05 -0300

    addreducer

---
+++ reducers/src/App.tsx
@@ -0,0 +1,44 @@
+import React, {useReducer} from 'react';
+import './App.css';
+
+type reducerState = {
+  count: number
+}
+
+type reducerAction = {
+  type: string
+}
+
+const initialState = { count: 0 };
+
+const reducer = (state: reducerState, action: reducerAction) => {
+  switch(action.type){
+    case "ADD":
+      return {...state, count: state.count + 1}
+    break;
+    case "DEL":
+      if(state.count > 0){
+        return {...state, count: state.count - 1}
+      }
+    break;
+    case "RESET": 
+      return initialState
+    break;
+  }
+  return state;
+}
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <div>
+      <p>{state.count}</p>
+      <button onClick={() => { dispatch({type: "ADD"})}}>Adicionar</button>
+      <button onClick={() => { dispatch({type: "DEL"})}}>Deletar</button>
+      <button onClick={() => { dispatch({type: "RESET"})}}>Resetar</button>
+    </div>
+  );
+}
+
+export default App;
